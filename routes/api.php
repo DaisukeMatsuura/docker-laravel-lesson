c661f2677c9ff6b1b4e06b14dab0305868f2b2b6
twil3 <twil3akine@gmail.com>
2024-09-02 15:39:12 +0900

    add API '/heathy'

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Routes::get('/health', function () {
+    return response()->json([
+        'message'=>'API is Healthy'
+    ]);
+});
